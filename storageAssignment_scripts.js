7aa73a46cac231c2dcf6b7f65be45471035cb3ac
AnjaliGupta-Zemoso <108920456+AnjaliGupta-Zemoso@users.noreply.github.com>
2022-07-13 17:46:43 +0530

    corrected the logic for localStorage

--- storageAssignment_scripts.js
+++ storageAssignment_scripts.js
@@ -1,10 +1,19 @@
 let i =0;
 let j= 100;
 function localScore() {
-    localStorage.setItem("local",i);
-    i++;
-    var count1 = localStorage.getItem('local'); 
-    document.getElementById("id1").innerHTML= count1;
+     if(localStorage.getItem('local') == null)
+    {
+        localStorage.setItem("local",1);
+        x = 1;
+    } else {
+        var x = localStorage.getItem('local');
+        var x = Number(x)+1;
+        localStorage.setItem("local",x);
+    }
+    
+    // i++;
+    // var count1 = localStorage.getItem('local'); 
+    document.getElementById("id1").innerHTML= x;
 }
 function sessionScore() {
     sessionStorage.setItem("session",j);
